353da38ff98f1665909d0f8ebca24edb9486780f
sfk <sfk9999@163.com>
2018-01-05 09:55:35 +0800

    new

--- src/components/nav/List.vue
+++ src/components/nav/List.vue
@@ -1,19 +1,34 @@
 <template>
-  <div>
-    123
+  <div class="conllection">
+      <a href="#!" class="collection-item"
+          v-for="gameName in gameNames"
+          @click="selected(gameName)"
+          :class="{active:activeName==gameName}"
+      >{{gameName}}</a>
   </div>
 </template>
 
 <script>
     export default{
         data(){
-            return {}
+            return {
+                gameNames:['1','2','3'],
+                activeName:''
+            }
+        },
+        methods:{
+            selected:function(gameName){
+                this.activeName=gameName;
+                console.log(this.activeName);
+            }
         }
     }
 
 </script>
 
 <style>
-
+.active{
+  color: white;
+}
 
 </style>
